42510b477dbd1538f3c454b979525db8a1c5a7a0
kremleva_aa <kremleva_aa@taximaxim.ru>
2020-04-24 15:26:16 +0500

    Connections refactirong

---
+++ ServicesStateMonitor/Infrastructure/TagHelpers/ConnectionTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ServicesStateMonitor.Infrastructure.TagHelpers
+{
+    public class ConnectionTagHelper : TagHelper
+    {
+        public string Start { get; set; }
+
+        public string End { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "line";
+            string lineId = Start + End;
+            string pointerId = Start + End + "Pointer";
+            output.Attributes.SetAttribute("id", lineId);
+            output.Attributes.SetAttribute("stroke", "black");
+
+            output.PostElement.AppendHtml($"<polygon id=\"{pointerId}\" fill=\"black\" />");
+        }
+    }
+}
